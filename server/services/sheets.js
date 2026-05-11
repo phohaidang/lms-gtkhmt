9fac0562dd3138fb75761567f31cb01f83326f20
Dang <phohaidang@gmail.com>
2026-05-11 22:55:02 +0700

    Fix: robust google private key parsing

--- server/services/sheets.js
+++ server/services/sheets.js
@@ -58,7 +58,9 @@ if (!IS_MOCK) {
   const auth = new google.auth.GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+      private_key: process.env.GOOGLE_PRIVATE_KEY 
+        ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/"/g, '') 
+        : undefined
     },
     scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file']
   });
